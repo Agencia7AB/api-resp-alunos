c32cc8115748219810ca77c2d4ddd83142684fe7
Jonathas <jonathaspereira673@gmail.com>
2025-04-25 15:58:09 -0300

    first commit

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = 3080;
 
 app.use(express.json());
 
-const API_URL = "https://unnichat.com.br/w/GJWhbBQHEFyfvYvpFAUu";
-const API_KEY = "fb6ab904-43fe-4d87-b180-dc8e9833a31e";
+const API_URL = "https://unnichat.com.br/a/lZ0coRozxRsT3vtSorSC";
+const API_KEY = "23bea959-2979-44d0-86a8-717f02f3cd3a";
 
 
 app.get("/", (req, res) => {
@@ -29,11 +29,10 @@ app.post("/cadastro/manychat", async (req, res) => {
     const formBody = new URLSearchParams();
     formBody.append("fullname", manychatNome || "");
     formBody.append("email", manychatEmail || "");
-    formBody.append("phone", "");
-    formBody.append("field[129]", "ManyChat");
-    formBody.append("field[130]", "Automação");
-    formBody.append("field[131]", "Captação");
-    formBody.append("field[132]", "AutomaçãoDeCadastro");
+    formBody.append("field[11]", "ManyChat");
+    formBody.append("field[12]", "Automação");
+    formBody.append("field[13]", "Captação");
+    formBody.append("field[14]", "AutomaçãoDeCadastro");
     formBody.append("u", "140");
     formBody.append("f", "140");
     formBody.append("s", "");
@@ -41,15 +40,77 @@ app.post("/cadastro/manychat", async (req, res) => {
     formBody.append("m", "0");
     formBody.append("act", "sub");
     formBody.append("v", "2");
-    formBody.append("or", "8946301f3aab4b1376755ea8480813fd");
+    formBody.append("or", "bbe4f7f90b97f28721f4a718421d02b8");
 
-    response = await fetch("https://brunobonibilia.activehosted.com/proc.php", {
+    response = await fetch("https://rayanepinto66712.activehosted.com/proc.php", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
       body: formBody,
     });
+
+    res.status(200).json({ message: "Success" });
+  } catch (error) {
+    console.error("Erro:", error);
+    res.status(500).json({ message: "Erro ao processar a requisição" });
+  }
+});
+
+// Rota para receber a requisição POST
+app.post("/cadastro/unnichat", async (req, res) => {
+
+  var API_KEY_AUTH = "e4e002d2-3b27-424c-aa40-0e10b733f984";
+
+  const data = req.body;
+  try {
+    let response;
+    let result;
+    const data = req.body.contact; // Acessa o objeto 'contact'
+    const { name, email, phoneNumber, id } = data; // Extrai os dados
+
+    console.log("Dados recebidos: ", data)
+
+    const formBody = new URLSearchParams();
+    formBody.append("fullname", name || ""); // Usa o nome do JSON
+    formBody.append("email", email || ""); // Usa o email do JSON
+    formBody.append("phone", phoneNumber || ""); // Usa o telefone do JSON
+    formBody.append("field[11]", "Unnichat");
+    formBody.append("field[12]", "Automação");
+    formBody.append("field[13]", "Captação");
+    formBody.append("field[14]", "AutomaçãoDeCadastro");
+    formBody.append("u", "149");
+    formBody.append("f", "149");
+    formBody.append("s", "");
+    formBody.append("c", "0");
+    formBody.append("m", "0");
+    formBody.append("act", "sub");
+    formBody.append("v", "2");
+    formBody.append("or", "bbe4f7f90b97f28721f4a718421d02b8");
+
+    response = await fetch("https://rayanepinto66712.activehosted.com/proc.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: formBody,
+    });
+
+    let payload = {
+      id: id,
+      tag_id: "gibeNqsomHdO0JBoFkUh"
+    }
+
+    var options = {
+      method: "post",
+      contentType: "application/json",
+      payload: JSON.stringify(payload),
+      headers: {
+        "Authorization": API_KEY_AUTH
+      }
+    };
+
+    result = await fetch("https://southamerica-east1-plataforma-unnichat-meta.cloudfunctions.net/unniapi/contact/addTag", options);
 
     res.status(200).json({ message: "Success" });
   } catch (error) {
@@ -80,9 +141,9 @@ app.post("/atualizacao/manychat", async (req, res) => {
     formBody.append("m", "0");
     formBody.append("act", "sub");
     formBody.append("v", "2");
-    formBody.append("or", "cb7d3bb7942749b52c97958cf1731e32");
+    formBody.append("or", "bbe4f7f90b97f28721f4a718421d02b8");
 
-    response = await fetch("https://brunobonibilia.activehosted.com/proc.php", {
+    response = await fetch("https://rayanepinto66712.activehosted.com/proc.php", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
@@ -97,11 +158,50 @@ app.post("/atualizacao/manychat", async (req, res) => {
   }
 });
 
+app.post("/cadastro/forms/fc", async (req, res) => {
+  const data = req.body;
+  try {
+    let response;
+    const { telefone, nome, email, utm_source, utm_medium, utm_campaing, utm_content } = data;
+
+    const formBody = new URLSearchParams();
+    formBody.append("fullname", nome || "");
+    formBody.append("email", email || "");
+    formBody.append("phone", telefone || "");
+    formBody.append("field[11]", utm_source || "");
+    formBody.append("field[12]", utm_medium || "");
+    formBody.append("field[13]", utm_campaing || "");
+    formBody.append("field[14]", utm_content || "");
+    formBody.append("u", "147");
+    formBody.append("f", "147");
+    formBody.append("s", "");
+    formBody.append("c", "0");
+    formBody.append("m", "0");
+    formBody.append("act", "sub");
+    formBody.append("v", "2");
+    formBody.append("or", "bbe4f7f90b97f28721f4a718421d02b8");
+
+    response = await fetch("https://rayanepinto66712.activehosted.com/proc.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: formBody,
+    });
+
+    res.status(200).json({ message: "Success" });
+  } catch (error) {
+    console.error("Erro:", error);
+    res.status(500).json({ message: "Erro ao processar a requisição" });
+  }
+});
 
 // Rota para processar a requisição diretamente
 app.post("/process-data", async (req, res) => {
   try {
     const receivedData = req.body;
+
+    console.log("Dados recebidos: ", receivedData);
 
     // Validação: Confirma que o JSON contém o parâmetro "telefone"
     if (!receivedData.telefone) {
@@ -113,12 +213,13 @@ app.post("/process-data", async (req, res) => {
       data: {
         user: {
           phoneNumber: receivedData.telefone,
-          name: "",
+          name: "Sem nome",
           email: ""
         }
       }
     };
 
+    console.log("Reqisição: ", requestBody);
     // Fazendo a requisição diretamente ao endpoint
     const response = await fetch(API_URL, {
       method: "POST",
@@ -127,6 +228,56 @@ app.post("/process-data", async (req, res) => {
     });
 
     const result = await response.json();
+    console.log("Resultado: ", result);
+
+    // Resposta final para o cliente
+    res.status(200).json({
+      message: "Requisição enviada com sucesso!",
+      response: result
+    });
+  } catch (error) {
+    console.error("Erro ao processar a requisição:", error.message);
+    res.status(500).json({
+      message: "Erro interno ao enviar a requisição.",
+      error: error.message
+    });
+  }
+});
+
+
+// Rota para processar a requisição diretamente
+app.post("/importacao", async (req, res) => {
+  try {
+    const receivedData = req.body;
+
+    console.log("Dados recebidos: ", receivedData);
+
+    // Validação: Confirma que o JSON contém o parâmetro "telefone"
+    if (!receivedData.whatsapp_phone) {
+      return res.status(400).json({ message: "Parâmetro 'telefone' não encontrado." });
+    }
+
+    const requestBody = {
+      apiKey: "4fa926ee-abf0-4033-95fb-b18002ac739d",
+      data: {
+        user: {
+          phoneNumber: receivedData.whatsapp_phone,
+          name: "Sem nome",
+          email: ""
+        }
+      }
+    };
+
+    console.log("Reqisição: ", requestBody);
+    // Fazendo a requisição diretamente ao endpoint
+    const response = await fetch("https://unnichat.com.br/a/rqROLyERi5m4IDsQGnLC", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(requestBody),
+    });
+
+    const result = await response.json();
+    console.log("Resultado: ", result);
 
     // Resposta final para o cliente
     res.status(200).json({
@@ -152,7 +303,7 @@ app.post("/integracao/clint", async (req, res) => {
       };
 
       // Enviando a requisição para o webhook
-      const response = await fetch("https://functions-api.clint.digital/endpoints/integration/webhook/870fd5a2-917e-48fc-bed6-e15372980303", {
+      const response = await fetch("https://functions-api.clint.digital/endpoints/integration/webhook/7ea35c16-a46a-4eae-ab04-eee1161268cd", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
@@ -162,7 +313,7 @@ app.post("/integracao/clint", async (req, res) => {
         throw new Error(`Erro ao enviar dados para o webhook: ${response.statusText}`);
       }
     }
-    
+
     res.status(200).json({ message: "Success" });
   } catch (error) {
     console.error("Erro:", error);
